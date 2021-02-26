3d4f75e3637e1a030bacca6f884bd4a11608f2c5
Matt <matt@mousybusiness.com>
2021-02-26 13:42:16 +0000

    fix: moved projectID reference to inside function call to fix post-load project updates

--- pkg/proj/proj.go
+++ pkg/proj/proj.go
@@ -7,13 +7,11 @@ import (
 	"os"
 )
 
-var (
-	projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
-)
-
 // used to get secrets from secret manager - for some reason projectId doesnt work, might be a security thing so resource id cant be guessed
 // expecting env var GOOGLE_CLOUD_PROJECT - AppEngine runtime variable
 func GetProjectNumber() (int64, error) {
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+
 	cloudresourcemanagerService, err := cloudresourcemanager.NewService(context.Background())
 	if err != nil {
 		//log.Fatalf("NewService: %v", err)
